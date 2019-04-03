35aadfa68e6d15437d1429f5f0848b71e3ef0ae2
mrstephenneal <stephen.neal14@gmail.com>
2019-04-03 12:46:50 -0400

    FIX issue with font registration

--- PillowImage/font/register.py
+++ PillowImage/font/register.py
@@ -7,7 +7,7 @@ from reportlab.pdfbase.ttfonts import TTFont
 
 def register_font(font='Vera.ttf'):
     """Register fonts for report labs canvas."""
-    directory = os.path.join(bundle_dir(), 'font')
+    directory = os.path.join(bundle_dir())
     ttfFile = resource_path(os.path.join(directory, font))
     if os.path.exists(ttfFile):
         pdfmetrics.registerFont(TTFont("Vera", ttfFile))
