dcfd9ed2a16822326a8ed7ad56d362cd5141144b
mrstephenneal <stephen.neal14@gmail.com>
2019-06-14 14:14:34 -0400

    ADD jpg image and testing suite

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,8 +2,13 @@ import os
 
 
 TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+
 IMG_NAME = 'floor plan.png'
 IMG_PATH = os.path.join(TEST_DATA_DIR, IMG_NAME)
+
+IMG_NAME_JPEG = 'elevation.jpg'
+IMG_PATH_JPEG = os.path.join(TEST_DATA_DIR, IMG_NAME)
+
 WTR_NAME = 'watermark.png'
 WTR_PATH = os.path.join(TEST_DATA_DIR, WTR_NAME)
 
