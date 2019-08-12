c1bcf74d13f6cc0a02547c1590869dfdeec1c859
mrstephenneal <stephen.neal14@gmail.com>
2019-08-12 10:22:34 -0400

    FIX test resultant file paths
     - all output files from tests are saved in the 'results' sub-folder

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,7 @@ import os
 
 
 TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+TEST_RESULTS_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 IMG_NAME = 'floor plan.png'
 IMG_PATH = os.path.join(TEST_DATA_DIR, IMG_NAME)
@@ -13,4 +14,4 @@ WTR_NAME = 'watermark.png'
 WTR_PATH = os.path.join(TEST_DATA_DIR, WTR_NAME)
 
 
-__all__ = ['IMG_PATH', 'WTR_PATH', 'TEST_DATA_DIR', 'IMG_NAME_JPEG', 'IMG_PATH_JPEG']
+__all__ = ['IMG_PATH', 'WTR_PATH', 'TEST_DATA_DIR', 'TEST_RESULTS_DIR', 'IMG_NAME_JPEG', 'IMG_PATH_JPEG']
