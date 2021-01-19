50cc1e32bbf1359fbd37102f4e5e8444346c55db
Stephen Neal <stephen.neal14@gmail.com>
2021-01-19 14:16:17 -0500

    REFACTOR setup.py by adding use of get_version() helper function
    ADD Python 3.6 or higher requirement.

--- setup.py
+++ setup.py
@@ -1,34 +1,43 @@
-import re
+import os
 from setuptools import setup, find_packages
 
-long_description = """
+NAME = 'PillowImage'
+
+DESCRIPTION = 'Pillow wrapper for quick image alterations.'
+
+LONG_DESCRIPTION = """
 Pure Python Pillow package wrapper.
 """
 
-# Retrieve version number
-VERSIONFILE = "PillowImage/_version.py"
-verstrline = open(VERSIONFILE, "rt").read()
-VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-mo = re.search(VSRE, verstrline, re.M)
-if mo:
-    verstr = mo.group(1)
-else:
-    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE))
+
+def get_version(package_name, version_file='_version.py'):
+    """
+    Retrieve the package version from a version file in the package root.
+
+    :param package_name:
+    :param version_file: version file name (inside package root)
+    :return: package version
+    """
+    filename = os.path.join(os.path.dirname(__file__), package_name, version_file)
+    with open(filename, 'rb') as fp:
+        return fp.read().decode('utf8').split('=')[1].strip(" \n'")
+
 
 setup(
+    name=NAME,
+    version=get_version(NAME),
+    packages=find_packages(),
+    python_requires='>3.6',
     install_requires=[
         'reportlab>=3.5.19',
         'Pillow>=7.0',
         'PyBundle>=1.0.6',
     ],
-    name='PillowImage',
-    version=verstr,
-    packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/mrstephenneal/PillowImage',
-    license='',
+    url='https://github.com/sfneal/PillowImage',
+    license='MIT',
     author='Stephen Neal',
     author_email='stephen@stephenneal.net',
-    description='Pillow wrapper for quick image alterations.',
-    long_description=long_description,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
 )
